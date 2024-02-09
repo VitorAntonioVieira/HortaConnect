9edbf2038502177519b4af1f6a878baf0e63b622
Lana Araújo <lana.academico2@gmail.com>
2024-02-09 09:49:52 -0300

    login

---
+++ login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <link rel="icon" href=" ./img/logo-hortalize-verde.png/"> -->
+    <link rel="stylesheet" href="css/login.css">
+    <title>Login</title>
+</head>
+<body>
+
+<div class="container">
+
+<!-- <form id="login-form" method="post" action=""> -->
+    <form action="">
+    <h1 id="logintitulo">Login</h1>
+
+    <input type="email" class="formzao" name="email" placeholder="Email" autocomplete="off"
+        id="email">
+    <br><br>
+    <input type="password" class="formzao" name="senha" placeholder="Senha" id="senha">
+    <i class="fa-regular fa-eye" id="show-password"></i><br><br>
+
+    <input type="submit" value="Entrar" id="botaologin">
+
+    <p id="naotenho">Não tem conta?</p>
+    <a class="cadastre-se" href="cadastro.php">Cadastre-se</a>
+
+    <p id=""><a class="esqueceur" href="redefinirsenha.php">Esqueci minha senha</a></p>
+
+    <a href="index.php">Voltar a página inicial</a>
+    </form>
+    </div>
+
+
+    
+</body>
+</html>

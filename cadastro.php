2e05798b8292296d81c34fcbc59fae0307e11703
Lana Araújo <lana.academico2@gmail.com>
2024-02-26 08:49:23 -0300

    cadastro

---
+++ cadastro.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/cadastro.css">
+    <title>Cadastro</title>
+</head>
+<body>
+<body>
+<!-- <div class="container"> -->
+
+<!-- <form id="login-form" method="post" action=""> -->
+    <form action="">
+    <img src="src/header-logo.png" class="logologin" alt="" srcset="">
+
+        <input type="email" class="formzao" name="email" placeholder="Email" autocomplete="off"
+        id="email"><br><br>
+        <input type="text" name="text" id="" placeholder="Nome Completo">
+        <br><br>
+        <input type="password" class="formzao" name="senha" placeholder="Senha" id="senha">
+        <i class="fa-regular fa-eye" id="show-password"></i><br><br>
+
+    <input type="submit" value="Cadastrar" id="botaocadastro">
+
+    <div class="container">
+        <a href="index.php" class="voltar">Voltar a página inicial</a>
+    </div>
+    </form>
+</body>
+</html>

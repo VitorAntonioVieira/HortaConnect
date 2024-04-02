546de67e8dc49c9d3eda12ac5c9dc7762d498775
alice-lima-dev <alice.assis004@gmail.com>
2024-04-02 11:51:30 -0300

    commit feito por professor wesley das alterações que alice fez 2 semanas atrás

--- crud/cadastro.php
+++ crud/cadastro.php
@@ -1,5 +1,5 @@
 <?php
- include "conexao.php";
+include "conexao.php";
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
     $nomeCompleto = $_POST['nome'];
@@ -16,27 +16,30 @@ $conn->close();
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/cadastro.css">
     <title>Cadastre-se</title>
 </head>
+
 <body>
-<div class="page">
-        <form action="#" method="POST" class="formLogin">
+    <div class="page">
+        <form action="" method="POST" class="formLogin">
             <h1>Cadastre-se</h1>
             <p>Digite os seus dados de acesso noa campos abaixo.</p>
             <label for="email">E-mail</label>
             <input type="email" placeholder="Insira seu e-mail" autofocus="true" name="email" />
-            <label for="email">Nome Completo</label>
-            <input name="nome" type="text" placeholder="Insira sue nome Completo" autofocus="true"  />
+            <label for="nome">Nome Completo</label>
+            <input name="nome" type="text" placeholder="Insira sue nome Completo" autofocus="true" />
             <label for="password">Senha</label>
-            <input type="password" placeholder="Digite sua senha" name="senha"/>
+            <input type="password" placeholder="Digite sua senha" name="senha" />
             <a href="loginnovo.php">Já tem acesso?</a>
             <a href="#">Esqueci minha senha</a>
-            <a href="./agradecimento.php"><input type="submit" value="Acessar" class="btn" /></a>
+            <a href="../agradecimento.php"><input type="submit" value="Acessar" class="btn" /></a>
         </form>
     </div>
 </body>
+
 </html>
--- crud/loginnovo.php
+++ crud/loginnovo.php
@@ -1,29 +1,30 @@
 <?php
 session_start();
-include 'conexao.php';
+include './conexao.php';
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $email = $_POST['email'];
     $senha = $_POST['senha'];
 
+    echo "$senha";
 
-    $sql_consulta = "SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha'";
+    $sql_consulta = "SELECT * FROM usuarios WHERE email = '$email'";
     $res = $conn->query($sql_consulta);
 
-
     if ($res->num_rows == 1) {
-        $funcionario = $res->fetch_assoc();
-        if (password_verify($senha, $usuario['senha'])) {
-            $_SESSION['funcionario_id'] = $usuario['id'];
-            $_SESSION['email'] = $usuario['email'];
+        // echo'teste';
+        $array_criado = $res->fetch_assoc();
+        // echo'teste2';
+        
+        // echo $senha_banco;
+        if (password_verify($senha, $array_criado['senha'])) {
             echo "logado";
+            $_SESSION['id'] = $array_criado['id'];
+            $_SESSION['email'] = $array_criado['email'];
+            header("Location:cadastro.php");
         }
-        ;
-
-
     } else {
-        setcookie("login", $login);
-        header("Location:index.php");
+        setcookie("email", $email);
         echo "erro ou logar";
     }
 }
@@ -34,26 +35,25 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
     <title>Login</title>
     <link rel="stylesheet" href="../css/loginnovo.css">
 </head>
 
 <body>
     <div class="page">
-        <form action="#" method="POST" class="formLogin">
+        <form action="" method="POST" class="formLogin">
             <h1>Login</h1>
             <p>Digite os seus dados de acesso no campo abaixo.</p>
             <label for="email">E-mail</label>
-            <input type="email" placeholder="Digite seu e-mail" autofocus="true" name="email" />
-            <label for="password">Senha</label>
-            <input type="password" placeholder="Digite sua senha" name="senha" />
+            <input type="email" placeholder="Digite seu e-mail" autofocus="true" id="email" name="email" />
+            <label for="senha">Senha</label>
+            <input type="password" placeholder="Digite sua senha" name="senha" id="senha" />
             <a href="cadastro.php">Não tem cadastro?</a>
             <a href="#">Esqueci minha senha</a>
-            <a href="./agradecimento.php"><input type="submit" value="Acessar" class="btn" /></a>
+            <input type="submit" value="Acessar" class="btn" />
         </form>
     </div>
-
 </body>
 
 </html>

75d9bf64f6d7459c0bdb4e5c8124e0790ef2179c
Vitorrrrrrrrr <vitoravs008@gmail.com>
2024-04-05 16:15:01 -0300

    commit (terminar)

--- index.php
+++ index.php
@@ -38,50 +38,68 @@
         </div>
     </nav>
 
-    <div class="splide__track">
-        <ul class="splide__list">
-            <li class="splide__slide">
-                <img class="imagem1" src="src/general/imagem1 (2).jpg" alt="Imagem de uma horta">
-            </li>
-        </ul>
-    </div>
-    <div class="display">
-        <div class="primeiro">
-            <img id="teste-fundo" src="./src/general/img-fundo.png" />
-            <div id="container">
-                <div class="box-info">
-                    <p id="informa">Informações</p>
-                    <div div="guia">
-                        <p id="guia-agricultor">Guia do agricultor</p>
-                        <p id="descricao">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                        </p>
-                    </div>
-                    <div div="guia">
-                        <p id="guia-agricultor">Soluções tecnologicas</p>
-                        <p id="descricao">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                        </p>
-                    </div>
-                    <div div="guia">
-                        <p id="guia-agricultor">Mercado interno</p>
-                        <p id="descricao">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                        </p>
-                    </div>
+    <section class="waves">
+        <div class="content">
+            <div class="content-block">
+                <img src="./src/general/logo-preto.png" alt="" class="logo panel" id="panel-img">
+                <div class="text-block">
+                    <h1 class="panel" id="panel-title">Hortalize</h1>
+                    <text class="panel" id="panel-text">"Transformando sementes de ideias, em colheitas de
+                        sucesso”</text>
                 </div>
-                <div class="box-info">
-                    <p id="informa">Posso ajudar?</p>
-                    <div div="guia">
-                        <p id="selecionar">Selecione as informações</p>
-                        <form>
-                            <input type="text" placeholder="Tipo de solo" name="solo" />
-                            <input type="text" placeholder="Cultura" name="cultura" />
-                            <input type="text" placeholder="Pragas" name="pragas" />
-                        </form>
-                        <button class="botao-form transitionScale" type="submit">Pesquisar</button>
+            </div>
+            <div class="content-block">
+                <img src="" alt="" class="floating-img" id="panel-img1">
+                <img src="" alt="" class="floating-img" id="panel-img2">
+                <img src="" alt="" class="floating-img" id="panel-img3">
+            </div>
+        </div>
+        <div class="onda" id="onda1"></div>
+        <div class="onda" id="onda2"></div>
+        <div class="onda" id="onda3"></div>
+        <div class="onda" id="onda4"></div>
+    </section>
+
+    <section>
+        <div class="display">
+            <div class="primeiro">
+                <!-- <img id="teste-fundo" src="./src/general/img-fundo.png" /> -->
+                <div id="container">
+                    <div class="box-info">
+                        <p id="informa">Informações</p>
+                        <div div="guia">
+                            <p id="guia-agricultor">Guia do agricultor</p>
+                            <p id="descricao">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                            </p>
+                        </div>
+                        <div div="guia">
+                            <p id="guia-agricultor">Soluções tecnologicas</p>
+                            <p id="descricao">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                            </p>
+                        </div>
+                        <div div="guia">
+                            <p id="guia-agricultor">Mercado interno</p>
+                            <p id="descricao">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="box-info">
+                        <p id="informa">Posso ajudar?</p>
+                        <div div="guia">
+                            <p id="selecionar">Selecione as informações</p>
+                            <form>
+                                <input type="text" placeholder="Tipo de solo" name="solo" />
+                                <input type="text" placeholder="Cultura" name="cultura" />
+                                <input type="text" placeholder="Pragas" name="pragas" />
+                            </form>
+                            <button class="botao-form transitionScale" type="submit">Pesquisar</button>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    </section>
+
     <div class="info-text">
         <h2>Bem vindos ao <span id="cor">HORTA CONNECT</span></h2>
         <p id="informacao-horta">Somos um projeto criado para ajudar produtores rurais, com o objetivo de maximizar o
@@ -128,10 +146,16 @@
         <br>
         <p>Siga nossas redes sociais!</p>
         <ul id="ul">
-          <img id="icone3" src="./src/icone/twitter.png">
-          <img id="icone3" src="./src/icone/instagram.png">
+            <img id="icone3" src="./src/icone/twitter.png">
+            <img id="icone3" src="./src/icone/instagram.png">
         </ul>
     </footer>
+    <script>
+        window.addEventListener("scroll", function () {
+            let header = document.querySelector("nav");
+            header.classList.toggle('.scrolled', window.scrollY > 0)
+        });
+    </script>
 </body>
 
 </html>

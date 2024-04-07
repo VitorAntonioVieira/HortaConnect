1dc6f77f13bbabefda01d33db778b5b12b6207d3
Lana Araújo <lana.academico2@gmail.com>
2024-04-06 22:54:15 -0300

    api chat gpt

--- js/chat.js
+++ js/chat.js
@@ -12,54 +12,48 @@ document.addEventListener("DOMContentLoaded", function() {
 
     searchButton.addEventListener("click", function() {
         sendQuestion();
-        console.log('oi')
     });
 
-    const OPENAI_API_KEY = "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";
+    const OPENAI_API_KEY = "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";
 
     function sendQuestion() {
         var sQuestion = inputQuestion.value;
 
         fetch("https://api.openai.com/v1/completions", {
-            method: "POST",
-            headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json",
-                Authorization: "Bearer " + OPENAI_API_KEY,
-            },
-            body: JSON.stringify({
-                prompt: sQuestion,
-                max_tokens: 2048, // tamanho da resposta
-                temperature: 0.5, // criatividade na resposta
-            }),
-        })
+    method: "POST",
+    headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + OPENAI_API_KEY,
+    },
+    body: JSON.stringify({
+        model: "gpt-3.5-turbo-instruct", // Remova o espaço em branco antes do identificador do modelo
+        prompt: sQuestion,
+        max_tokens: 2048, // tamanho da resposta
+        temperature: 0.5, // criatividade na resposta
+    }),
+})
+
+        
         .then(response => response.json())
         .then(json => {
-            if (result.value) result.value += "\n";
-
             if (json.error?.message) {
-                result.value += `Error: ${json.error.message}`;
+                result.value = `Error: ${json.error.message}`;
             } else if (json.choices?.[0].text) {
-                var text = json.choices[0].text || "Sem resposta";
-
-                result.value += "Chat GPT: " + text;
+                result.value = "Chat GPT: " + json.choices[0].text;
             }
-
-            result.scrollTop = result.scrollHeight;
         })
-        .catch(error => console.error("Error:", error))
+        .catch(error => {
+            console.error("Error:", error);
+            result.value = "Ocorreu um erro ao processar sua pergunta.";
+        })
         .finally(() => {
             inputQuestion.value = "";
             inputQuestion.disabled = false;
             inputQuestion.focus();
         });
 
-        if (result.value) result.value += "\n\n\n";
-
-        result.value += `Eu: ${sQuestion}`;
-        inputQuestion.value = "Carregando...";
+        result.value = "Carregando...";
         inputQuestion.disabled = true;
-
-        result.scrollTop = result.scrollHeight;
     }
 });

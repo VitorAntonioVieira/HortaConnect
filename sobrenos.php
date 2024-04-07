7dd87c69a8ce17708decd575c52e624c28cb0676
Lana Araújo <lana.academico2@gmail.com>
2024-04-06 22:54:00 -0300

    responsividade sobre nós

--- sobrenos.php
+++ sobrenos.php
@@ -40,8 +40,8 @@
                 mais do que uma empresa, somos uma missão: ajudar produtores rurais a prosperar, impulsionando o uso
                 inteligente da tecnologia em suas operações, enquanto preservamos e protegemos nosso meio ambiente.
             <p></p>
-
-            <p id="info"><span id="text">Nosso Propósito:</span><br>
+            <span id="text">Nosso Propósito:</span>
+            <p id="info">
                 Somos um projeto criado para ajudar produtores rurais, com o objetivo de maximizar o uso da tecnologia
                 nas
                 produções, auxiliando no impacto econômico e social, de modo que não haja impacto no meio ambiente.
@@ -50,7 +50,8 @@
         </div>
         <img id="imagem-exemplo" src="./src/general/logo-verde.png">
     </div>
-    <p id="info2"><span id="text">Como Funcionamos:</span><br>
+    <span id="text">Como Funcionamos:</span>
+    <p id="info2">
         Na HORTA CONNECT, combinamos expertise em tecnologia com profundo conhecimento agrícola.
         Desenvolvemos
         soluções inovadoras, desde sistemas de monitoramento de culturas até ferramentas de gestão de recursos
@@ -62,26 +63,35 @@
         <div class="img-integrantes">
             <div class="teste-img">
                 <img id="alice" src="./src/integrantes/foto-alice.jpg">
-                <p id="descricao-alice">Alice de Lima</p>
-                <p id="descricao2">Back-end e Segurança</p>
+                <div class="descricao">
+                    <p class="nome">Alice de Lima</p>
+                    <p class="funcao">Back-end e Segurança</p>
+                </div>
             </div>
             <div class="teste-img">
-                <img id="alice" src="./src/integrantes/foto-lana.jpg">
-                <p id="descricao-alice">Lana Araújo</p>
-                <p id="descricao-lana">Desing</p>
+                <img id="lana" src="./src/integrantes/foto-lana.jpg">
+                <div class="descricao">
+                    <p class="nome">Lana Araújo</p>
+                    <p class="funcao">Design</p>
+                </div>
             </div>
             <div class="teste-img">
-                <img id="alice" src="./src/integrantes/foto-rafael.jpg">
-                <p id="descricao-alice">Rafael Henrique</p>
-                <p id="descricao-rafael">Analista</p>
+                <img id="rafael" src="./src/integrantes/foto-rafael.jpg">
+                <div class="descricao">
+                    <p class="nome">Rafael Henrique</p>
+                    <p class="funcao">Analista</p>
+                </div>
             </div>
             <div class="teste-img">
-                <img id="alice" src="./src/integrantes/foto-vitor.jpg">
-                <p id="descricao-alice">Vitor Antonio</p>
-                <p id="descricao-vitor">Product owner</p>
+                <img id="vitor" src="./src/integrantes/foto-vitor.jpg">
+                <div class="descricao">
+                    <p class="nome">Vitor Antonio</p>
+                    <p class="funcao">Product owner</p>
+                </div>
             </div>
         </div>
     </div>
+    
     <footer>
         <img id="logo-footer" src="./src/general/header-logo.png">
         <p>Av. Monsenhor Theodomiro Lobo, 100 - Parque Res. Maria Elmira,<br> Caçapava - SP</p>

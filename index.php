b82980bf2c821c26e793be3765b464d5b0dfe33e
Vitor Antonio Vieira da Silva <vitoravs008@gmail.com>
2024-04-08 03:17:34 -0300

    oiiii

--- index.php
+++ index.php
@@ -12,11 +12,42 @@
         rel="stylesheet">
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://unpkg.com/scrollreveal"></script>
     <title>Horta Connect</title>
 </head>
 
 <body>
+    <nav class="navbar fixed-top">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#"><img src=".src/general/header-logo.svg" alt=""></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
+                aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar"
+                aria-labelledby="offcanvasNavbarLabel">
+                <div class="offcanvas-header">
+                    <h5 class="offcanvas-title" id="offcanvasNavbarLabel"><img src=".src/general/header-logo.svg" alt=""></h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body">
+                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="./loginnovo.php"><button class="transitionScale login-btn">Login</button></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
     <nav class="header">
         <div class="info">
             <div class="logo-nav-box">
@@ -45,13 +76,20 @@
                 <div class="text-block">
                     <h1 class="panel" id="panel-title">HortaConnect</h1>
                     <text class="panel" id="panel-text">"Transformando sementes de ideias, em colheitas de
-                        sucesso”</text>
+                        sucesso”
+                    </text>
                 </div>
             </div>
-            <div class="content-block">
-                <img src="" alt="" class="floating-img" id="panel-img1">
-                <img src="" alt="" class="floating-img" id="panel-img2">
-                <img src="" alt="" class="floating-img" id="panel-img3">
+            <div class="content-block flex">
+                <div class="floating" id="panel-img1">
+                    <img src="" alt="">
+                </div>
+                <div class="floating" id="panel-img2">
+                    <img src="" alt="">
+                </div>
+                <div class="floating" id="panel-img3">
+                    <img src="" alt="">
+                </div>
             </div>
         </div>
         <div class="onda" id="onda1"></div>
@@ -60,30 +98,31 @@
         <div class="onda" id="onda4"></div>
     </section>
     <section class="display">
-        <div id="container">
+        <div class="container">
             <div class="box-info hidden">
                 <h2 class="box-title">Serviços</h2>
-                <div div="guia">
-                    <p id="guia-agricultor">Guia do agricultor</p>
-                    <p id="descricao">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                <div class="guia">
+                    <p class="box-index">Guia do Agricultor</p>
+                    <p class="box-desc">Tire suas dúvidas em nosso guia com informações apuradas.
                     </p>
                 </div>
-                <div div="guia">
-                    <p id="guia-agricultor">Soluções tecnologicas</p>
-                    <p id="descricao">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                <div class="guia">
+                    <p class="box-index">Soluções Tecnológicas</p>
+                    <p class="box-desc">Entregamos inovação e precisão. Você produz mais e desperdiça menos.
                     </p>
                 </div>
-                <div div="guia">
-                    <p id="guia-agricultor">Mercado interno</p>
-                    <p id="descricao">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                <div class="guia">
+                    <p class="box-index">Mercado interno</p>
+                    <p class="box-desc">Alcance a sua comunidade local, proporcionando uma alimentação saudável e
+                        sincera.
                     </p>
                 </div>
             </div>
             <div class="box-info hidden">
                 <script src="./js/chat.js" defer></script>
                 <h2 class="box-title">Posso ajudar?</h2>
-                <div id="guia">
-                    <p id="selecionar">Tire suas dúvidas!</p><br>
+                <div class="guia">
+                    <p class="box-sub">Tire suas dúvidas!</p><br>
                     <textarea class="box-input" id="inputQuestion" placeholder="Pergunte algo aqui!"></textarea>
                     <textarea class="box-input" id="result" rows="10" disabled placeholder="Resposta da sua pergunta"
                         style="height: 20%;"></textarea>
@@ -135,19 +174,20 @@
             <p class="box-text center">Nosso atendimento resolve seu problema em até 1 dia útil</p>
         </div>
     </section>
-    <a href="solucoes.php" class="hidden center" style="text-decoration: none; margin-top: -15vh"><input
-            class="botao-form transitionScale" type="button" value="Confira nossos produtos"></a>
-    <div class="hidden" style="padding-top: 5vh; padding-bottom: 15vh">
+    <a href="solucoes.php" class="m-height hidden center" style="text-decoration: none;">
+        <input class="botao-form transitionScale" type="button" value="Confira nossos produtos">
+    </a>
+    <div class="mockup-container hidden">
         <img id="app-logo" src="./src/general/app-logo.webp" alt="Logo Aplicativo HortaConnect">
         <section class="content" style="padding-top: 0">
             <div class="content-block center">
                 <img id="mockup" src="./src/general/mockup-app.jpg"
                     alt="Imagem de celular com o Aplicativo HortaConnect">
             </div>
-            <div class="content-block" style="padding-top: 10vh;">
+            <div class="content-block" id="mockup-content">
                 <h2 class="content-title">Baixe nosso <strong id="strong">aplicativo</strong>!</h2>
                 <p class="content-text">Tenha acesso à todas funcionalidades e serviços da HortaConnect.</p>
-                <div class="content" style="width: 100%; padding-left: 0;">
+                <div class="content" id="mockup-badges">
                     <div class="content-block ">
                         <img class="badge" src="./src/general/gplay-badge.png" alt="Baixe na GooglePlay Store">
                     </div>
@@ -195,34 +235,30 @@
                             </div>
                         </div>
                         <div class="footer-box">
-                            <form action="/enviar" method="post" style="display:flex; justify-content:center; flex-direction:column">
+                            <h3 class="box-title" id="footer-form">Entre em contato</h3>
+                            <form action="/enviar" method="post"
+                                style="display:flex; justify-content:center; flex-direction:column; align-items:center; width:100%">
                                 <input class="form-input" type="text" id="nome" name="nome" placeholder="Nome" required>
-                                <input class="form-input" type="email" id="email" name="email" placeholder="Email" required>
-                                <input class="form-input" id="mensagem" name="mensagem" style="height: 10vh;" placeholder="Digite sua mensagem..." required></input>
+                                <input class="form-input" type="email" id="email" name="email" placeholder="Email"
+                                    required>
+                                <input class="form-input" id="mensagem" name="mensagem" style="height: 10vh;"
+                                    placeholder="Digite sua mensagem..." required></input>
                                 <button type="submit" class="botao-form transitionScale">Enviar</button>
                             </form>
                         </div>
                     </div>
                 </div>
-                <div class="footer-element" style="display:flex; flex-direction:column; align-items:center">
+                <div class="footer-element" style="display:flex; flex-direction:column;">
                     <div class="footer-divisor"></div>
                     <p class="content-text" style="color:#ffffff">Tech Motion 2024 - SESISENAI SP</p>
                 </div>
             </div>
         </div>
-        <!-- <img id="logo-footer" src="./src/general/header-logo.png">
-        
-        <br>
-        <p>Email: hortalize.2024@gmail.com</p>
-        <p>Telefone: (12) 3653-1943</p>
-        <br>
-        <p>Siga nossas redes sociais!</p>
-        <ul id="ul">
-            <img id="icone3" src="./src/icone/twitter.png">
-            <img id="icone3" src="./src/icone/instagram.png">
-        </ul> -->
     </footer>
     <script src="./js/main.js" defer></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
 </body>
 
 </html>

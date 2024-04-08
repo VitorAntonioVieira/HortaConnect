e9fa215fb68450ae4ac9c39366522a22e44a42bd
Vitorrrrrrrrr <vitoravs008@gmail.com>
2024-04-08 08:16:54 -0300

    nav escondida

--- index.php
+++ index.php
@@ -12,14 +12,12 @@
         rel="stylesheet">
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://unpkg.com/scrollreveal"></script>
     <title>Horta Connect</title>
 </head>
 
 <body>
-    <nav class="navbar fixed-top">
+    <!-- <nav class="navbar fixed-top">
         <div class="container-fluid">
             <a class="navbar-brand" href="#"><img src=".src/general/header-logo.svg" alt=""></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
@@ -47,7 +45,7 @@
                 </div>
             </div>
         </div>
-    </nav>
+    </nav> -->
     <nav class="header">
         <div class="info">
             <div class="logo-nav-box">
@@ -256,9 +254,6 @@
         </div>
     </footer>
     <script src="./js/main.js" defer></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-        crossorigin="anonymous"></script>
 </body>
 
 </html>

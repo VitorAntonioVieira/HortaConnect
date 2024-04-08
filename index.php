49fbad4e16879a4053c00b73d99dbfb020ed323a
alice-lima-dev <alice.assis004@gmail.com>
2024-04-08 13:46:32 -0300

    sdbsd

--- index.php
+++ index.php
@@ -35,35 +35,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 </head>
 
 <body>
-    <!-- <nav class="navbar fixed-top">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#"><img src=".src/general/header-logo.svg" alt=""></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
-                aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar"
-                aria-labelledby="offcanvasNavbarLabel">
-                <div class="offcanvas-header">
-                    <h5 class="offcanvas-title" id="offcanvasNavbarLabel"><img src=".src/general/header-logo.svg" alt=""></h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                </div>
-                <div class="offcanvas-body">
-                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="./loginnovo.php"><button class="transitionScale login-btn">Login</button></a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </nav> -->
     <nav class="header">
         <div class="info">
             <div class="logo-nav-box">

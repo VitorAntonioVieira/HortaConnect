2a67ccce4e2a06e6e29c87beb6673539f1a6e47f
Vitorrrrrrrrr <vitoravs008@gmail.com>
2024-04-08 13:47:44 -0300

    dfd

--- index.php
+++ index.php
@@ -12,14 +12,12 @@
         rel="stylesheet">
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://unpkg.com/scrollreveal"></script>
     <title>Horta Connect</title>
 </head>
 
 <body>
-    <nav class="navbar">
+    <nav class="navbar fixed-top" style="display:none">
         <div class="container-fluid">
             <a class="navbar-brand" href="#"><img src=".src/general/header-logo.svg" alt=""></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"

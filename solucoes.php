6ed3d5c2ed727e37661fc3e6ca96e876789a2d22
alice-lima-dev <alice.assis004@gmail.com>
2024-09-05 08:01:28 -0300

    fsf

--- solucoes.php
+++ solucoes.php
@@ -1,6 +1,6 @@
 <?php
-    session_start();
-    include './crud/conexao.php';
+session_start();
+include './crud/conexao.php';
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -27,7 +27,7 @@
             <!-- Navbar content -->
         </div>
     </nav>
-    
+
     <!-- Header -->
     <nav class="header">
         <div class="info">
@@ -78,43 +78,8 @@
             ?>
         </div>
     </div>
-    <h1>Soluções Tecnológicas</h1>
-    <p class="subtitulo">Irrigação Inteligente</p>
-    <hr id="hr2" />
-    <div class="container">
-        <div class="ume">
-            <?php
-            $sql = "SELECT * FROM irrigador";
-            $resultado = $conn->query($sql);
-            if ($resultado->num_rows > 0) {
-                while ($row = $resultado->fetch_assoc()) {
-                    // Incrementing modal ID
-                    $modal_id = $row['id_irrigador'];
-                    echo "<div class='solução'>";
-                    echo "<div class='img-sensor'>";
-                    echo "<img id='img-sensor' src='" . $row['foto'] . "'>";
-                    echo "</div>";
-                    echo "<div id='informação'>";
-                    echo "<h3>" . $row['nome'] . "</h3>";
-                    echo "<p>" . $row['valor'] . "</p>";
-                    echo "<button id='openModal{$modal_id}Btn' class='botao-form transitionScale'>Saiba Mais</button>";
-                    echo "</div>";
-                    echo "<div id='myModal{$modal_id}' class='modal'>";
-                    echo "<div class='modal-content'>";
-                    echo "<span class='close' onclick='fechar()' id='closeModal{$modal_id}'>&times;</span>";
-                    echo "<img id='img-modal' src='src/general/cisterna-pequena.jpg'>";
-                    echo "<div id='alinhar'>";
-                    echo "<h2></h2>";
-                    echo "<p>" . $row['descricao'] . "</p>";
-                    echo "<h4>" . $row['valor'] . "</h4>";
-                    echo "</div>";
-                    echo "</div>";
-                    echo "</div>";
-                    echo "</div>";
-                }
-            }
-            ?>
-        </div>
+
+    </div>
     </div>
 
     <!-- JavaScript -->
